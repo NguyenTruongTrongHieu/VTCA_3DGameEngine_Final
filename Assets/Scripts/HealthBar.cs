00d2ee0bac5cc532e17c9b6c33100742acaa94b2
thien2408387 <quangthientran3142@gmail.com>
2025-03-14 08:39:18 +0700

    Add Color Change To Healthbar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,15 +7,23 @@ public class HealthBar : MonoBehaviour
 {
     public Slider healthSlider;
 
+    public Gradient gradient;
+
+    public Image fill;
+
     public void SetMaxHealth(int health)
     {
         healthSlider.maxValue = health;
         healthSlider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         healthSlider.value = health;
+
+        fill.color = gradient.Evaluate(healthSlider.normalizedValue);
     }
 
 }
